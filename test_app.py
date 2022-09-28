c71a5c74c0796f87d4f9872d9786ef49bbb84121
Suborna Nath <subornadnath@gmail.com>
2022-09-27 23:18:39 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -8,3 +8,7 @@ from application import app
 def test_home_page():
     response = app.test_client().get('/')
     assert response.status_code == 200
+    
+def test_button():
+    response = app.test_client().get('youtube.com')
+    assert response.status_code == 404
