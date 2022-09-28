9710401c547e3d3430b3f6a59fcd60e4b8867c56
Suborna Nath <subornadnath@gmail.com>
2022-09-27 23:46:21 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -9,6 +9,10 @@ def test_home_page():
     response = app.test_client().get('/')
     assert response.status_code == 200
     
-def test_button():
+def test_unsaved_url():
     response = app.test_client().get('youtube.com')
     assert response.status_code == 404
+    
+def test_saved_url():
+    response = app.test_client().get('/house')
+    assert response.status_code == 200
