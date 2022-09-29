a0b84f76480f7b6858b261b734cc7e0a3c6fd419
suborna <subornadnath@gmail.com>
2022-09-29 11:41:48 -0400

    configured cypress test

--- cypress_test/cypress.config.js
+++ cypress_test/cypress.config.js
@@ -1,7 +1,12 @@
-{
-  "reporter": "junit",
-  "reporterOptions": {
-    "mochaFile": "results/cypress-report.xml",
-    "toConsole": true
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://url-shortner-dev.us-east-1.elasticbeanstalk.com/',
+    reporter: 'junit',
+    reporterOptions: {
+      mochaFile: 'results/cypress-this.reporter.xml',
+      toConsole: true
+    }
   }
-}
+})
